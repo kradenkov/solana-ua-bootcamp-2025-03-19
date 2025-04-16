edff8c4952ad828497297939b6a3641f42579df5
Oleksandr Kradenkov <kradenkov@gmail.com>
2025-04-16 12:55:54 +0300

    task2.1-lesson 4

---
+++ practice-4/send-sol.ts
@@ -0,0 +1,42 @@
+import "dotenv/config";
+import {
+  Keypair,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  SystemProgram,
+  Transaction,
+  clusterApiUrl,
+  Connection,
+  sendAndConfirmTransaction
+} from "@solana/web3.js";
+
+let privateKey = process.env["PK"];
+if (privateKey === undefined) {
+  console.log("Add PK to .env!");
+  process.exit(1);
+}
+const asArray = Uint8Array.from(JSON.parse(privateKey));
+const sender = Keypair.fromSecretKey(asArray);
+
+const connection = new Connection(clusterApiUrl("devnet"));
+
+console.log(`🔑 Our public key is: ${sender.publicKey.toBase58()}`);
+
+
+const recipient = new PublicKey("FDpn2tAhRfmnQGV6J62iP44TPxFCaLXLgUsWpJiYTEUY");
+console.log(`💸 Attempting to send 0.01 SOL to ${recipient.toBase58()}...`);
+
+const transaction = new Transaction();
+
+const sendSolInstruction = SystemProgram.transfer({
+  fromPubkey: sender.publicKey,
+  toPubkey: recipient,
+  lamports: 0.01 * LAMPORTS_PER_SOL,
+});
+transaction.add(sendSolInstruction);
+
+const signature = await sendAndConfirmTransaction(connection, transaction, [
+  sender,
+]);
+
+console.log(`✅ Transaction confirmed, signature: ${signature}!`);
